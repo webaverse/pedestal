c6aa1330e3f1f2c1b9dcae8709784f85113cd1d0
Avaer Kazmer <a@modules.io>
2021-09-25 21:36:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import * as THREE from 'three';
-import {renderer, camera, runtime, world, universe, physics, ui, rig, app, appManager, popovers} from 'app';
+// import {renderer, camera, runtime, world, universe, physics, ui, rig, app, appManager, popovers} from 'app';
+import metaversefile from 'metaversefile';
+const {useApp, usePhysics} = metaversefile;
 
 const localVector = new THREE.Vector3();
 const localMatrix = new THREE.Matrix4();
 
-const _clone = o => JSON.parse(JSON.stringify(o));
+// const _clone = o => JSON.parse(JSON.stringify(o));
 
-const physicsId = physics.addBoxGeometry(new THREE.Vector3(0, -1, 0), new THREE.Quaternion(), new THREE.Vector3(1000, 1, 1000), false);
+export default () => {
+  const app = useApp();
 
-function mod(a, n) {
-  return ((a%n)+n)%n;
-}
-const gridHelper = new THREE.GridHelper(10, 10);
-app.object.add(gridHelper);
+  const physicsId = physics.addBoxGeometry(new THREE.Vector3(0, -1, 0), new THREE.Quaternion(), new THREE.Vector3(1000, 1, 1000), false);
 
-/* function animate(timestamp, frame, referenceSpace) {
-  const now = Date.now();
-}
-renderer.setAnimationLoop(animate); */
+  function mod(a, n) {
+    return ((a%n)+n)%n;
+  }
+  const gridHelper = new THREE.GridHelper(10, 10);
+  app.add(gridHelper);
+
+  /* function animate(timestamp, frame, referenceSpace) {
+    const now = Date.now();
+  }
+  renderer.setAnimationLoop(animate); */
+  
+  useCleanup(() => {
+    physics.removeGeometry(physicsId);
+  });
+  
+  return app;
+};
